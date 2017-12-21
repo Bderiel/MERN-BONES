35c0123596305df1f9a6b7ea4806bd20123241f0
Bderiel <bderiel@gmail.com>
2017-12-21 14:26:50 -0500

    router works again;

--- server/index.js
+++ server/index.js
@@ -26,22 +26,22 @@ passport.deserializeUser((id, done) => {
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../public')));
 
-// app.use(session({
-//   secret: process.env.SESSION_SECRET || 'Trabajo',
-//   resave: false,
-// }));
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'Trabajo',
+  resave: false,
+}));
 
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use(passport.initialize());
-// app.use(passport.session()); // persistent login sessions
-// app.use(cookieParser()); // read cookies (needed for auth)
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(cookieParser()); // read cookies (needed for auth)
 
 
-// /* *    API and auth routes   * */
-// app.use('/auth', require('./auth'));
-// app.use('/api', require('./api'));
+/* *    API and auth routes   * */
+app.use('/auth', require('./auth'));
+app.use('/api', require('./api'));
 
 
 /* *   Sends index.html  * */
@@ -49,12 +49,12 @@ app.use('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public/index.html'));
 });
 
-// /* *   Error handling endware    * */
-// app.use((err, req, res) => {
-//   console.error(err);
-//   console.error(err.stack);
-//   res.status(err.status || 500).send(err.message || 'Internal server error.');
-// });
+/* *   Error handling endware    * */
+app.use((err, req, res) => {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error.');
+});
 
 
 mongoose.connect('mongodb://localhost/mydatabase', (err) => {
